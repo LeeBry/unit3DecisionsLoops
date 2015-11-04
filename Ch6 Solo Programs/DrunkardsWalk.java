0ac1dea1d57a8a338396ff0a751b1d9840f4505b
LeeBry <bylee@NN123-HA-20.HS.sd203.org>
2015-11-04 14:11:28 -0600

    Starting Monty Carlo
    Intergralssssssssss

---
+++ Ch6 Solo Programs/DrunkardsWalk.java
@@ -0,0 +1,42 @@
+
+import java.util.Random;
+public class DrunkardsWalk
+{
+    public static void DrunkardsWalk()
+    {
+        int x= 0;
+        int y= 0;
+        Random gen= new Random();
+        int direction;
+        int walked= 100;
+        for (int i =0; i !=walked; i++)
+        {
+            direction= gen.nextInt(4)+1;
+            //1= North y+1
+            //2= East x+1
+            //3= South y-1
+            //4= West x-1
+            if (direction ==1)
+            {
+                y+=1;
+                System.out.println("Walked North and you have reached point ("+x+","+y+")");
+            }
+            else if(direction ==2)
+            {
+                x+=1;
+                System.out.println("Walked East and you have reached point ("+x+","+y+")");
+            }
+            else if(direction ==3)
+            {
+                y-=1;
+                System.out.println("Walked South and you have reached point ("+x+","+y+")");
+            }
+            else if(direction ==4)
+            {
+                x-=1;
+                System.out.println("Walked West and you have reached point ("+x+","+y+")");
+            }
+        }
+        System.out.println("You have walked 100 intersections, and your final point is("+x+","+y+")");       
+    }
+}
